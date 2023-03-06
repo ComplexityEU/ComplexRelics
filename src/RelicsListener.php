64f35a9c1320fb08ff4197e7d41cb5c0a0f172b1
Duo Incure <duoincuredev@gmail.com>
2023-03-06 07:24:22 +0000

    Use `lookupBlockAliases` instead of being a dumbass
    🧍‍♂️

--- src/RelicsListener.php
+++ src/RelicsListener.php
@@ -2,17 +2,13 @@
 
 namespace DuoIncure\Relics;
 
-use pocketmine\block\Block;
-use pocketmine\block\CoralBlock;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerItemUseEvent;
 use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\item\StringToItemParser;
 use function array_map;
 use function in_array;
-use function method_exists;
 use function rand;
-use function str_replace;
-use function strtolower;
 
 class RelicsListener implements Listener {
 
@@ -37,16 +33,16 @@ class RelicsListener implements Listener {
         if($rFunctions !== null && $nbt->getTag(RelicFunctions::RELIC_TAG) !== null){
             $relicType = $nbt->getTag(RelicFunctions::RELIC_TAG)->getValue();
             switch($relicType){
-                case "common":
+                case RelicFunctions::TYPE_COMMON:
                     $rFunctions->giveRelicReward($player, $item, RelicFunctions::TYPE_COMMON);
                     break;
-                case "rare":
+                case RelicFunctions::TYPE_RARE:
                     $rFunctions->giveRelicReward($player, $item, RelicFunctions::TYPE_RARE);
                     break;
-                case "epic":
+                case RelicFunctions::TYPE_EPIC:
                     $rFunctions->giveRelicReward($player, $item, RelicFunctions::TYPE_EPIC);
                     break;
-                case "legendary":
+                case RelicFunctions::TYPE_LEGENDARY:
                     $rFunctions->giveRelicReward($player, $item, RelicFunctions::TYPE_LEGENDARY);
                     break;
             }
@@ -62,51 +58,43 @@ class RelicsListener implements Listener {
         $player = $ev->getPlayer();
         $config = $this->plugin->getConfig();
 
-        $blockName = str_replace(" ", "_", strtolower($this->getBlockTypedName($ev->getBlock())));
         $configBlocks = (array)$config->get("block-ids", []);
         $blockNameArr = array_map('strtolower', $configBlocks);
+        $blockAliases = StringToItemParser::getInstance()->lookupBlockAliases($ev->getBlock());
         $configWorlds = (array)$config->get("worlds", []);
         $levelName = $player->getWorld()->getDisplayName();
         $rFunctions = $this->plugin->getRelicFunctions();
         if($rFunctions !== null) {
-            if (in_array($blockName, $blockNameArr, true) && ($configWorlds[0] == "*" or in_array($levelName, $configWorlds, true))) {
-                $commonChance = (int)$config->getNested("common.chance", 10);
-                $rareChance = (int)$config->getNested("rare.chance", 5);
-                $epicChance = (int)$config->getNested("epic.chance", 3);
-                $legendaryChance = (int)$config->getNested("legendary.chance", 1);
-                //Credit to @SOF3 and @benda95280 for this chance system.
-                $chance = rand(1, 200);
-                if ($chance <= $commonChance) {
-                    $rFunctions->giveRelic($player, RelicFunctions::TYPE_COMMON, 1);
-                } else {
+            foreach ($blockNameArr as $blockName) {
+                if (in_array($blockName, $blockAliases, true) && ($configWorlds[0] == "*" || in_array($levelName, $configWorlds, true))) {
+                    $commonChance = (int)$config->getNested("common.chance", 10);
+                    $rareChance = (int)$config->getNested("rare.chance", 5);
+                    $epicChance = (int)$config->getNested("epic.chance", 3);
+                    $legendaryChance = (int)$config->getNested("legendary.chance", 1);
+                    //Credit to @SOF3 and @benda95280.
+                    $chance = rand(1, 200);
+                    if ($chance <= $commonChance) {
+                        $rFunctions->giveRelic($player, RelicFunctions::TYPE_COMMON, 1);
+                        return;
+                    }
                     $chance -= $commonChance;
                     if ($chance <= $rareChance) {
                         $rFunctions->giveRelic($player, RelicFunctions::TYPE_RARE, 1);
-                    } else {
-                        $chance -= $rareChance;
-                        if ($chance <= $epicChance) {
-                            $rFunctions->giveRelic($player, RelicFunctions::TYPE_EPIC, 1);
-                        } else {
-                            $chance -= $epicChance;
-                            if ($chance <= $legendaryChance) {
-                                $rFunctions->giveRelic($player, RelicFunctions::TYPE_LEGENDARY, 1);
-                            }
-                        }
+                        return;
                     }
+                    $chance -= $rareChance;
+                    if ($chance <= $epicChance) {
+                        $rFunctions->giveRelic($player, RelicFunctions::TYPE_EPIC, 1);
+                        return;
+                    }
+                    $chance -= $epicChance;
+                    if ($chance <= $legendaryChance) {
+                        $rFunctions->giveRelic($player, RelicFunctions::TYPE_LEGENDARY, 1);
+                        return;
+                    }
+                    return;
                 }
             }
         }
-    }
-
-    public function getBlockTypedName(Block $block): string{
-        if(method_exists($block, 'getColor')) {
-            $name = $block->getColor()->getDisplayName() . " " . $block->asItem()->getVanillaName();
-        } elseif($block instanceof CoralBlock) {
-            $name = $block->getCoralType()->getDisplayName() . " " . $block->asItem()->getVanillaName();
-        } else {
-            $name = $block->asItem()->getVanillaName();
-        }
-
-        return $name;
     }
 }
